427f209818aebe74ccad42578dcc226d0f19e7ce
Omar Hashi <omar3166435@gmail.com>
2025-08-21 12:00:19 +0300

    change allowed hosts for railway server.

--- backend/loundry/settings/prod.py
+++ backend/loundry/settings/prod.py
@@ -12,7 +12,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['simple-loundry-production.up.railway.app']
 
 # Database configuration for Railway
 DATABASES = {
